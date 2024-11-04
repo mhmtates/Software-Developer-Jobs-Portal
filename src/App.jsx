d29521f595ee8076f8778a6db4700a5058efa39f
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2024-11-04 05:32:16 +0300

    New components and pages are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Route>
+  )
+);
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Hero title='Yazılım Geliştirici Ol' subtitle='Yazılım İş İlanları Burada'/>
-      <HomeCards/>
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,31 @@
-
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+  const activeLink = ({isActive}) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2': 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
     return (
-        <nav className='bg-indigo-700 border-b border-indigo-500'>
+        <nav className='bg-cyan-700 border-b border-cyan-500'>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
             <div className='flex h-20 items-center justify-between'>
               <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-                <a className='flex flex-shrink-0 items-center mr-4' href='/'>
+                <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
                   <img className='h-10 w-auto' src={logo} alt='React Jobs' />
                   <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                      DevJobsPlatform
                   </span>
-                </a>
+                </NavLink>
                 <div className='md:ml-auto'>
                   <div className='flex space-x-6 justify-items-end'>
-                    <a href='/' className='font-bold text-2xl text-white'>AnaSayfa</a>
-                    <a href='/jobs.html' className='font-bold text-2xl text-white '>İşler</a>
-                    <a href='add-job.html' className='font-bold text-2xl text-white'>Yeni Bir İş Ekle</a>
+                    <NavLink to='/' className={activeLink}>
+                    AnaSayfa
+                    </NavLink>
+                    <NavLink to='/jobs' className={activeLink}>
+                    İşler
+                    </NavLink>
+                    <NavLink to='add-job' className={activeLink}>
+                    Yeni Bir İş Ekle
+                    </NavLink>
                   </div>
                 </div>
               </div>
